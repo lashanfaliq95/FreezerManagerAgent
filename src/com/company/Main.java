bbca7418f233d99598999af4d5c413d960ed0574
lashanfaliq95 <lashanfaliq@gmail.com>
2018-03-25 08:52:22 +0530

    Complete event publishing for all devices

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,49 +1,92 @@
 package com.company;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.*;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 public class Main implements Runnable {
     private String deviceId;
-    public Main(String deviceId){
-        this.deviceId=deviceId;
+
+    public Main(String deviceId) {
+        this.deviceId = deviceId;
     }
+
     Random rand = new Random();
-    public static void main(String[] args) throws IOException, InterruptedException  {
+    public static String accessToken = "a6a22578-d1e2-3651-a97d-2c3c088c2799";
+public static final String deviceType="FreezerManager";
+    public static void main(String[] args) throws IOException, InterruptedException {
+         ArrayList<String> devices=new ArrayList<>();
+        JsonParser jsonParser = new JsonParser();
+        String temp = getDevices();
+        JsonObject jsonPayload = jsonParser.parse(temp).getAsJsonObject();
+        JsonArray allDevices = jsonPayload.get("devices").getAsJsonArray();
+        for (JsonElement device : allDevices) {
+            JsonObject tempDevice = device.getAsJsonObject();
+            if((tempDevice.get("type").getAsString()).equals(deviceType)){
+                devices.add(tempDevice.get("deviceIdentifier").getAsString());
+            }
+            }
+        System.out.println(devices.size());
+        Thread newThread;
+        for(int i=0;i<devices.size();i++){
+             newThread= new Thread(new Main(devices.get(i)));
+             newThread.start();  //should be start();
+        }
 
-
-        Thread newThread = new Thread(new Main("Freezer17"));
-        newThread.run();  //should be start();
 
     }
+
     @Override
-    public void run(){
+    public void run() {
         String type = "application/json; charset=UTF-8";
-        String accessToken="639bc2d0-97cf-328a-a238-bfbcea848e43";
         while (true) {
             int temp = rand.nextInt(50) + 1, humidity = rand.nextInt(100) + 1, powerConsumption = rand.nextInt(50) + 1;
             String json = "{\"temperature\":" + temp + ",\"humidity\":" + humidity + ",\"powerConsumption\":" + powerConsumption + "}";
             byte[] out = json.getBytes(StandardCharsets.UTF_8);
-         try{
-            URL u = new URL("http://localhost:8280/api/device-mgt/v1.0/device/agent/events/publish/FreezerManager/" + deviceId);
-            HttpURLConnection conn = (HttpURLConnection) u.openConnection();
-            conn.setDoOutput(true);
-            conn.setRequestProperty("authorization", " Bearer " + accessToken);
-            conn.setRequestProperty("Content-Type", type);
-            conn.setRequestMethod("POST");
-            OutputStream os = conn.getOutputStream();
-            os.write(out);
-            os.flush();
-            System.out.println(conn.getResponseMessage());
-            TimeUnit.SECONDS.sleep(5);}
-            catch (Exception e){
-    e.printStackTrace();
+            try {
+                URL u = new URL("http://localhost:8280/api/device-mgt/v1.0/device/agent/events/publish/FreezerManager/" + deviceId);
+                System.out.println(deviceId);
+                HttpURLConnection conn = (HttpURLConnection) u.openConnection();
+                conn.setDoOutput(true);
+                conn.setRequestProperty("authorization", " Bearer " + accessToken);
+                conn.setRequestProperty("Content-Type", type);
+                conn.setRequestMethod("POST");
+                OutputStream os = conn.getOutputStream();
+                os.write(out);
+                os.flush();
+                System.out.println(conn.getResponseMessage());
+                TimeUnit.SECONDS.sleep(5);
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
+    }
+
+    private static String getDevices() throws IOException {
+        StringBuffer result = new StringBuffer();
+        URL url = new URL("http://localhost:8280/api/device-mgt/v1.0/devices");
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestProperty("authorization", " Bearer " + accessToken);
+        conn.setRequestMethod("GET");
+        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String line;
+        while ((line = rd.readLine()) != null) {
+            result.append(line);
+        }
+        rd.close();
+        System.out.println(result);
+        return result.toString();
     }
 
 }
